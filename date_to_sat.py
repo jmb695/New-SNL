82abe790f2da24d55707386c75816a4fee1a42a6
jmb695 <59187926+jmb695@users.noreply.github.com>
2020-05-04 13:56:29 -0400

    Update date_to_sat.py

--- date_to_sat.py
+++ date_to_sat.py
@@ -1,56 +1,113 @@
-import requests
-from bs4 import BeautifulSoup
-import date_to_sat as ds
+from datetime import date
 
-# Sets the page to crawl, set the correct date for the upcoming Saturday
-url = "https://www.tvpassport.com/tv-listings/stations/nbc-network-eastern/1227/"+ds.get_sat()
-response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-html = response.content
-
-soup = BeautifulSoup(html, "lxml")
-text = str(soup.find("body").get_text)
-
-#determines if the snl episode for the week is new or not
-def new_snl(html):
-    #cuts html down to just the snl episode
-    snl_ind = html.index('11:00 PM')
-    lp_ind = html.index('1:00:00')
-    shorter_html = html[snl_ind:lp_ind]
-    if 'data-new_show="' in shorter_html:
-        new_index = shorter_html.index('data-new_show="')
+#finds todays date and determines the date of the following saturday
+def get_sat():
+    #gets todays date
+    today = date.today()
+    weekday = today.weekday()
+    s_today = str(today)
+    #weekday = 6 signals it is a saturday
+    if weekday != 6:
+        #splits, converts date to numbers
+        year, mo, d = s_today.split('-')
+        d_num = int(d)
+        mo_num = int(mo)
+        year_num = int(year)
+        #finds how many days until the next saturday
+        day_to_sat = 6 - weekday
+        #these are months with 31 days
+        if mo_num == 1 or 3 or 5 or 7 or 8 or 10:
+            if d_num + day_to_sat > 31:
+                #takes the end of the month into account, finds date if saturday is in the next month
+                d_num = d_num + day_to_sat - 31
+                mo_num += 1
+                d_str = str(d_num)
+                mo_str = str(mo_num)
+                #formats the date, month into the same format as the target url
+                if len(d_str) < 2:
+                    d_str = '0'+d_str
+                if len(mo_str) < 2:
+                    mo_str = '0'+mo_str
+                #returns in proper formatting
+                return str(year_num)+"-"+mo_str+"-"+d_str
+            else:
+                #if its not the end of the month...
+                d_num = d_num + day_to_sat
+                d_str = str(d_num)
+                mo_str = str(mo_num)
+                if len(d_str) < 2:
+                    d_str = '0'+d_str
+                if len(mo_str) < 2:
+                    mo_str = '0'+mo_str
+                return str(year_num)+"-"+mo_str+"-"+d_str
+        #and repeat with other months...
+        if mo_num == 4 or 6 or 9 or 11:
+            if d_num + day_to_sat > 30:
+                d_num = d_num + day_to_sat - 30
+                mo_num += 1
+                d_num = d_num + day_to_sa
+                d_str = str(d_num)
+                mo_str = str(mo_num)
+                if len(d_str) < 2:
+                    d_str = '0'+d_str
+                if len(mo_str) < 2:
+                    mo_str = '0'+mo_str
+                return str(year_num)+"-"+mo_str+"-"+d_str
+            else:
+                d_num = d_num + day_to_sat
+                d_num = d_num + day_to_sa
+                d_str = str(d_num)
+                mo_str = str(mo_num)
+                if len(d_str) < 2:
+                    d_str = '0'+d_str
+                if len(mo_str) < 2:
+                    mo_str = '0'+mo_str
+                return str(year_num)+"-"+mo_str+"-"+d_str
+        if mo_num == 2:
+            if d_num + day_to_sat > 28:
+                d_num = d_num + day_to_sat - 28
+                mo_num += 1
+                d_num = d_num + day_to_sa
+                d_str = str(d_num)
+                mo_str = str(mo_num)
+                if len(d_str) < 2:
+                    d_str = '0'+d_str
+                if len(mo_str) < 2:
+                    mo_str = '0'+mo_str
+                return str(year_num)+"-"+mo_str+"-"+d_str
+            else:
+                d_num = d_num + day_to_sat
+                d_num = d_num + day_to_sa
+                d_str = str(d_num)
+                mo_str = str(mo_num)
+                if len(d_str) < 2:
+                    d_str = '0'+d_str
+                if len(mo_str) < 2:
+                    mo_str = '0'+mo_str
+                return str(year_num)+"-"+mo_str+"-"+d_str
+        #takes the end of the year into account
+        if mo_num == 12:
+            if d_num + day_to_sat > 30:
+                d_num = d_num + day_to_sat - 30
+                mo_num = 1
+                year_num += 1
+                d_num = d_num + day_to_sa
+                d_str = str(d_num)
+                mo_str = str(mo_num)
+                if len(d_str) < 2:
+                    d_str = '0'+d_str
+                if len(mo_str) < 2:
+                    mo_str = '0'+mo_str
+                return str(year_num)+"-"+mo_str+"-"+d_str
+            else:
+                d_num = d_num + day_to_sat
+                d_num = d_num + day_to_sa
+                d_str = str(d_num)
+                mo_str = str(mo_num)
+                if len(d_str) < 2:
+                    d_str = '0'+d_str
+                if len(mo_str) < 2:
+                    mo_str = '0'+mo_str
+                return str(year_num)+"-"+mo_str+"-"+d_str
     else:
-        return "TBD"
-    #a "1" here indicates that the show is new
-    if shorter_html[new_index+15] == '1':
-        return True
-    else:
-        return False
-
-#this finds who the guests are of the week's episode
-def snl_guests(html):
-    #cuts the html down to just the show
-    snl_ind = html.index('11:00 PM')
-    lp_ind = html.index('1:00:00')
-    shorter_html = html[snl_ind:lp_ind]
-    #finds where the guests are in the html
-    if 'data-guest="' in shorter_html:
-        guest_ind = shorter_html.index('data-guest="')
-    else:
-        return "TBD"
-    guest_str = shorter_html[guest_ind+12:]
-    guest_end = guest_str.index('"')
-    guests = guest_str[:guest_end]
-    #splits host and musical guest
-    host, music_guest = guests.split(',')
-    return host, music_guest
-
-#prints whether or not snl is new this week and who the guests are
-def snl_this_week():
-    if new_snl(text)==True:
-        print("SNL is new this week! The host is "+snl_guests(text)[0]+", and the musical guest is"+snl_guests(text)[1]+".")
-    elif new_snl(text)==False:
-        print("SNL is a rerun this week. The host is "+snl_guests(text)[0]+", and the musical guest is"+snl_guests(text)[1]+".")
-    else:
-        print("SNL is still TBD this week.")
-
-snl_this_week()
+        return s_today
