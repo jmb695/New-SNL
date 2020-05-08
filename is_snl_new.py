09908e3460f5f292e3b0bf33332f007e5a69f6a4
jmb695 <59187926+jmb695@users.noreply.github.com>
2020-05-08 15:18:11 -0400

    Update is_snl_new.py

--- is_snl_new.py
+++ is_snl_new.py
@@ -30,14 +30,15 @@ def new_snl(html):
 #this finds who the guests are of the week's episode
 def snl_guests(html):
     #cuts the html down to just the show
-    snl_ind = html.index('11:00 PM')
-    lp_ind = html.index('1:00:00')
+    snl_ind = html.index('11:30 PM')
+    lp_ind = html.index('1:00 AM')
     shorter_html = html[snl_ind:lp_ind]
     #finds where the guests are in the html
-    if 'data-guest="' in shorter_html:
+    if 'data-guest="' in shorter_html and shorter_html[shorter_html.index('data-guest="')+12] != '"':
         guest_ind = shorter_html.index('data-guest="')
+    #fixes a bug where guest data is empty. This seems to be the case for COVID episodes
     else:
-        return "TBD"
+        return ['TBD', 'TBD']
     guest_str = shorter_html[guest_ind+12:]
     guest_end = guest_str.index('"')
     guests = guest_str[:guest_end]
